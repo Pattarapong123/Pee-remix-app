f1db5f58009e4f5fa7a07d72eff4e36e68e0676b
Pattarapong123 <peesteam456@gmail.com>
2024-11-30 12:22:33 +0700

    week3

---
+++ app/routes/profile.tsx
@@ -0,0 +1,35 @@
+export default function MyProfile(){
+    return (
+        <>
+        <div className="p-5 m-5 border-2 border-red-600 rounded">
+            <h1 className="text-x1 font-bold text-amber-700">Profile</h1>
+            <ul className="list-disc list-inside"> </ul>
+            <h1>
+             Pattarapong Tangngam
+                  </h1>
+               <strong>
+                  026640491611-3
+           </strong>
+        
+        </div>
+        <MyEducation />
+        </>
+    );
+}
+
+function MyEducation(){
+    return(
+        <div className="p-5 m-5 border-2 border-red-600 rounded">
+            <h1 className="text-x1 font-bold text-amber-700">Educations</h1>
+            <ul className="list-disc list-inside">
+                <li>Information Tecnology,Rmutto, 2567-Present</li>
+                <li>Xxxx,xxxx,999-999</li>
+                <li>Xxxx,yyyy,999-999</li>
+
+            </ul>
+        </div>
+
+    );
+}
+
+//export default MyProfile
